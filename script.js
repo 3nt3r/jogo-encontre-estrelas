4b4206a6ceec9f5cd29b83fb65ba70fe32361e04
Jefferson <jeffersonbds_barros@hotmail.com>
2019-09-02 12:46:49 -0300

    Refatoração de código

--- script.js
+++ script.js
@@ -1,36 +1,49 @@
-var aleatorio = parseInt(Math.random() * (9 - 2) + 2);
-var verifica = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-var ponto = 0;
+var sorteioPosicaoEstrela = parseInt((Math.random() * 9) + 2);
+var quadrados = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+var pontos = 0;
 
-function primeiro(prim){
-	document.getElementById("d" + prim).style.backgroundImage = "url('estrela.png')";
-	verifica[0] = 1;
+function primeiraJogada(){
+	document.getElementById("d1").style.backgroundImage = "url('estrela.png')";
+	quadrados[0] = 1;
 }
 
 function jogo(div){
 
-	if(verifica[0] === 0){
+	if(quadrados[0] === 0){
+
 		alert("Clique no primeiro quadrado!");
 		return false;
-	} else if (parseInt(div) === aleatorio && verifica[parseInt(div)-1] === 0){
+
+	} else if (parseInt(div) === sorteioPosicaoEstrela){
+
 		document.getElementById("d" + div).style.backgroundImage = "url('estrela.png')";
-		if (verifica[aleatorio-1] != 1){
-		ponto += 10;
+
+		if (quadrados[sorteioPosicaoEstrela-1] != 1){
+			pontos += 10;
 		}
-		verifica[parseInt(div)-1] = 1;
-	} else if (verifica[parseInt(div)-1] !== 1) {
+		quadrados[parseInt(div)-1] = 1;
+
+	} else if (quadrados[parseInt(div)-1] !== 1) {
+
 		var r = parseInt(Math.random() * (255 - 0) + 0);
 		var g = parseInt(Math.random() * (255 - 0) + 0);
 		var b = parseInt(Math.random() * (255 - 0) + 0);
+
 		document.getElementById("d" + div).style.backgroundColor = "rgb(" + r + ", " + g + " ," + b + ")";
 		document.getElementById("d" + div).style.backgroundImage = "url()";
-		if (verifica[aleatorio-1] != 1){
-		ponto -= 1;
+
+		if (quadrados[sorteioPosicaoEstrela-1] != 1){
+			pontos -= 1;
 		}
+
 	}
-	verifica[parseInt(div)-1] = 1;
-	document.getElementById("pontos").innerHTML=ponto;
-	if (verifica[aleatorio-1] == 1){
-		document.getElementById("mensagem").innerHTML= "Parabéns! Você Acertou!";
+
+	quadrados[parseInt(div)-1] = 1;
+
+	document.getElementById("pontos").innerHTML = pontos;
+
+	if (quadrados[sorteioPosicaoEstrela-1] == 1){
+		document.getElementById("mensagem").innerHTML = "Parabéns, encontrou a estrela!";
 	}	
+
 }
